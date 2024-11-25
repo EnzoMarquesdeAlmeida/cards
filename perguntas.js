0f9a033c53106811b5126fdf339594f097442243
EnzoMarquesdeAlmeida <00001111488745sp@al.educacao.sp.gov.br>
2024-11-24 21:46:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Meow',
+    'Meo meow?',
+    'Meow!!!!'
+)
+
+criaCartao(
+    'Meow',
+    'Meow Meow Meow Meow?',
+    'Meow!!!'
+)
+
+criaCartao(
+    'Meow',
+    'Meow Meow Meow?',
+    'Meow!'
+)
+
+criaCartao(
+    'Meow ',
+    'Meow Meow Meow?',
+    'Meow/?'
+)
